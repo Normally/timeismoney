663e2a15d0f8c6518e25e042a19b233a1fe9a23d
Basil Safwat <bsafwat@gmail.com>
2016-01-19 16:17:01 +0000

    Move conversion function to TIM.convert

--- TIM.convert.js
+++ TIM.convert.js
@@ -1,3 +1,77 @@
 TIM.convert = (function() {
-  console.log("CONVERT!!!");
+
+  // d is seconds in that value, set for 5 days a week, 8 hours a day
+  var moneyToTime = function(money) {
+    var delta = parseFloat(money) / oneSecondWage(),
+      arr = [],
+      str = "",
+      count = 0,
+      flag;
+
+    // Calculate exact year, months, weeks, days etc... push them into an array.
+
+    for (var key in offsets) {
+      var value = Math.floor(delta / offsets[key].d);
+      var obj = {
+        key: key,
+        value: value
+      };
+      arr.push(obj);
+      delta -= value * offsets[key].d;
+    }
+
+    // Round up values according to max, rounding down is not nessecary due to the way we read them back later.
+
+    for (var i = arr.length - 1; i >= 0; i--) {
+      var a = arr[i];
+      if (a.value > offsets[a.key].max) {
+        arr[i - 1].value += 1;
+        a.value = 0;
+      }
+    }
+
+    // Loop through and pick just one or two of the highest values
+
+    for (var key in arr) {
+      var k = arr[key];
+      if ((flag && !k.value) || (flag && count > 1)) {
+        break;
+      }
+      if (k.value) {
+        str += (count > 0 ? ", " : "") + k.value + " " + k.key + (k.value > 1 ? 's' : '');
+        flag = true;
+        count++;
+      }
+    }
+    return str;
+  };
+
+  var replaceMoneyWithTime = function(text) {
+    var re = /\£\d{0,3}(,?\d+)?(.?\d+)(K|k|M|m)?/; // http://regexr.com/3cduh
+    var re_strip = /[^0-9.kKmM]/g;
+    var result;
+    var matches  = re.exec(text);
+    if (matches) {
+      var match = matches[0];
+      var cleaned = match.replace(re_strip, '');
+      if (/(k|K)$/.test(cleaned)) {
+        cleaned = cleaned.replace(/(k|K)$/, '');
+        cleaned = parseInt(cleaned) * Math.pow(10,3);
+      } else if (/(m|M)$/.test(cleaned)) {
+        cleaned = cleaned.replace(/(m|M)$/, '');
+        cleaned = parseInt(cleaned) * Math.pow(10,6);
+      }
+      var time = moneyToTime(cleaned);
+      result = matches.input.replace(match, time);
+    }
+    else { result = text; }
+    return result;
+  };
+
+  return {
+    moneyToTime:moneyToTime,
+    replaceMoneyWithTime:replaceMoneyWithTime
+  };
+
+
 })();
--- tests.js
+++ tests.js
@@ -1,64 +1,64 @@
 QUnit.test( "Check money to time conversion works", function( assert ) {
-  var time = moneyToTime(5);
+  var time = TIM.convert.moneyToTime(5);
   assert.equal( time, "6 minutes, 14 seconds" );
 });
 
 QUnit.test( "Replace a substring", function( assert ) {
   var before = "£1000 a week";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "2 days, 4 hours a week");
 });
 
 QUnit.test( "Match a number with decimals", function( assert ) {
   var before = "£1000.00";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "2 days, 4 hours");
 });
 
 QUnit.test( "Match a number with decimals as a substring", function( assert ) {
   var before = "£1000.00 a week";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "2 days, 4 hours a week");
 });
 
 QUnit.test( "Correctly parse a number with commas", function( assert ) {
   var before = "£10,000";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "1 month, 1 week");
 });
 
 QUnit.test( "Handle K", function( assert ) {
   var before = "£100K per year";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "1 year per year");
 });
 
 QUnit.test( "Handle k", function( assert ) {
   var before = "£100k per year";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "1 year per year");
 });
 
 QUnit.test( "Handle M", function( assert ) {
   var before = "£100M per year";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "1000 years per year");
 });
 
 QUnit.test( "Handle m", function( assert ) {
   var before = "£100m per year";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "1000 years per year");
 });
 
 QUnit.test( "Handle £25.5M", function( assert ) {
   var before = "£25.5M per year";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "250 years per year");
 });
 
 QUnit.test( "Match a number with decimals", function( assert ) {
   var before = "£1000.00";
-  var after  = replaceMoneyWithTime(before);
+  var after  = TIM.convert.replaceMoneyWithTime(before);
   assert.equal(after, "2 days, 4 hours");
 });
